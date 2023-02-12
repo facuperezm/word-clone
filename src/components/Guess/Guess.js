05ce3ce880c14d3cdb1d2ab0744785a2f865ddec
facuperezm <facundoperezmontalvo@gmail.com>
2023-02-12 16:41:11 -0300

    app: exercise 4 done

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const classname = status ? `cell ${status}` : "cell";
+  return <span className={classname}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
   return (
     <>
       <p className="guess">
         {range(5).map((num) => {
           return (
-            <span className="cell" key={num}>
-              {value[num] || " "}
-            </span>
+            <Cell
+              key={num}
+              letter={result ? result[num].letter : undefined}
+              status={result ? result[num].status : undefined}
+            />
           );
         })}
       </p>
